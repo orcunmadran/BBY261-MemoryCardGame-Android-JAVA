82344f8abeb1bac1c73c97134317180cd2bd07db
orcunmadran <orcunmadran@gmail.com>
2016-12-02 15:49:14 +0300

    Android projesi oluşturuldu

---
+++ app/src/main/java/net/madran/bby261/memorycardgame/MainActivity.java
@@ -0,0 +1,13 @@
+package net.madran.bby261.memorycardgame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
